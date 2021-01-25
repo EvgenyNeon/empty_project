121130849c58e9b8a4eb0e2d73c4795798b74724
panichkin <panichkin@stilsoft.ru>
2021-01-25 12:38:51 +0300

    ghg

--- em/pt/empty_project/empty_project-0.2.0/target/debug/deps/empty_project-26d2eb02c2f9aa7e.d
+++
@@ -1,7 +0,0 @@
-E:\rigel\src\empty_project\target\package\empty_project-0.2.0\target\debug\deps\empty_project-26d2eb02c2f9aa7e.rmeta: src\lib.rs
-
-E:\rigel\src\empty_project\target\package\empty_project-0.2.0\target\debug\deps\libempty_project-26d2eb02c2f9aa7e.rlib: src\lib.rs
-
-E:\rigel\src\empty_project\target\package\empty_project-0.2.0\target\debug\deps\empty_project-26d2eb02c2f9aa7e.d: src\lib.rs
-
-src\lib.rs:
