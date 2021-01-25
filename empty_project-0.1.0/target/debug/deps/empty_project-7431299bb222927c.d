25380db1f85c47abf10a160bb5a2492d93a3802d
panichkin <panichkin@stilsoft.ru>
2021-01-25 12:21:08 +0300

    234235

---
+++ empty_project-0.1.0/target/debug/deps/empty_project-7431299bb222927c.d
@@ -0,0 +1,7 @@
+E:\rigel\src\empty_project\target\package\empty_project-0.1.0\target\debug\deps\empty_project-7431299bb222927c.rmeta: src\lib.rs
+
+E:\rigel\src\empty_project\target\package\empty_project-0.1.0\target\debug\deps\libempty_project-7431299bb222927c.rlib: src\lib.rs
+
+E:\rigel\src\empty_project\target\package\empty_project-0.1.0\target\debug\deps\empty_project-7431299bb222927c.d: src\lib.rs
+
+src\lib.rs:
---
+++ empty_project-0.1.0/target/debug/libempty_project.d
@@ -0,0 +1,1 @@
+E:\rigel\src\empty_project\target\package\empty_project-0.1.0\target\debug\libempty_project.rlib: E:\rigel\src\empty_project\target\package\empty_project-0.1.0\src\lib.rs
